c0c9e590dc08c838af403f7e9c030651c2d28681
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-09-01 13:52:15 +0200

    Moved HamlPlugin to hatak.

--- src/konwentor/application/init.py
+++ src/konwentor/application/init.py
@@ -1,5 +1,5 @@
 from hatak.application import Application
-from hatak.plugins.plugin import Plugin
+from hatak.plugins.haml import HamlPlugin
 
 from konwentor.auth.plugin import AuthPlugin
 from konwentor.menu.plugin import MenuPlugin
@@ -7,17 +7,6 @@ from konwentor.flashmsg.plugin import FlashMessagePlugin
 from konwentor.forms.plugin import FormPlugin
 
 from .routes import make_routes
-
-
-class HamlPlugin(Plugin):
-
-    def before_config(self):
-        extensions = self.settings.get('jinja2.extensions', [])
-        extensions.append('hamlish_jinja.HamlishExtension')
-        self.settings['jinja2.extensions'] = extensions
-
-    def add_to_registry(self):
-        self.config.add_jinja2_renderer('.haml')
 
 
 main = Application('konwentor', make_routes)
