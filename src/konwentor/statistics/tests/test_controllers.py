bfc161b06d535de2d13ec622fd24e95084b79401
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-04-03 19:18:03 +0200

    Test for statistics controller.

--- src/konwentor/statistics/tests/test_controllers.py
+++ src/konwentor/statistics/tests/test_controllers.py
@@ -1,3 +1,6 @@
+from pytest import fixture, yield_fixture
+from mock import patch
+
 from hatak.testing import ControllerFixture
 from haplugin.sql.testing import DatabaseFixture
 
@@ -23,6 +26,10 @@ class TestStatisticsController(ControllerFixture):
 
 class TestStatisticsSqls(ControllerFixture, DatabaseFixture):
 
+    @fixture(autouse=True)
+    def setUp(self, fixtures, data):
+        data['convent'] = fixtures['Convent']['first']
+
     def _get_controller_class(self):
         return StatisticsController
 
@@ -31,78 +38,94 @@ class TestStatisticsSqls(ControllerFixture, DatabaseFixture):
         borrows = controller.get_borrows()
         assert borrows == []
 
-#     def test_get_borrows(self):
-#         self.controller.data = {'convent': fixtures['Convent']['second']}
-#         borrows = self.controller.get_borrows()
-#         self.assertEqual(
-#             [
-#                 fixtures['GameBorrow'][3],
-#                 fixtures['GameBorrow'][4]],
-#             borrows)
+    def test_get_borrows(self, controller, fixtures):
+        controller.data = {'convent': fixtures['Convent']['second']}
+        borrows = controller.get_borrows()
+        assert borrows == [
+            fixtures['GameBorrow'][3],
+            fixtures['GameBorrow'][4]
+        ]
 
-#     def test_add_all_games(self):
-#         self.controller.add_all_games()
-#         result = self.controller.data['statistics'][0]
-#         self.assertEqual(
-#             {
-#                 'name': 'Różnych gier',
-#                 'value': 2,
-#             },
-#             result,
-#             '3 copies of the same game nad 4 copy of another makes 2 '
-#             'uniqe games')
+    def test_add_all_games(self, controller, fixtures, data):
+        data['convent'] = fixtures['Convent']['first']
+        data['statistics'] = []
+        controller.add_all_games()
+        result = controller.data['statistics'][0]
+        assert result == {
+            'name': 'Różnych gier',
+            'value': 2,
+        }
 
-#     def test_add_all_copies(self):
-#         self.controller.add_all_copies()
-#         result = self.controller.data['statistics'][0]
-#         self.assertEqual(
-#             {
-#                 'name': 'Sztuk gier',
-#                 'value': 7,
-#             },
-#             result,
-#             '3 copies of the same game nad 4 copy of another makes 7 games')
+    def test_add_all_copies(self, controller, fixtures, data):
+        """
+        3 copies of the same game nad 4 copy of another makes 7 games
+        """
+        data['statistics'] = []
+        controller.add_all_copies()
+        result = controller.data['statistics'][0]
+        assert result == {
+            'name': 'Sztuk gier',
+            'value': 7,
+        }
 
-#     def test_add_all_people(self):
-#         self.controller.add_all_people()
+    def test_add_all_people(self, controller, fixtures, data):
+        data['statistics'] = []
+        controller.add_all_people()
 
-#         self.assertEqual(
-#             {
-#                 'name': 'Ilość różnych osób',
-#                 'value': 2,
-#             },
-#             self.data['statistics'][0])
+        assert data['statistics'][0] == {
+            'name': 'Ilość różnych osób',
+            'value': 2,
+        }
 
-#     def test_add_top_games(self):
-#         self.controller.add_top_games()
+    def test_add_top_games(self, controller, fixtures, data):
+        controller.add_top_games()
 
-#         self.assertEqual(
-#             [(2, 'first'), (1, 'second')], self.data['games'])
+        assert data['games'] == [(2, 'first'), (1, 'second')]
 
-#     def test_add_top_people(self):
-#         self.controller.add_top_people()
+    def test_add_top_people(self, controller, data):
+        controller.add_top_people()
 
-#         self.assertEqual(
-#             [
-#                 ('Franek', 'Kimono', 2),
-#                 ('Ten', 'Drugi', 1),
-#             ],
-#             self.data['peoples'])
+        assert data['peoples'] == [
+            ('Franek', 'Kimono', 2),
+            ('Ten', 'Drugi', 1),
+        ]
 
-#     def test_make(self):
-#         self.add_mock_object(self.controller, 'verify_convent')
-#         self.add_mock_object(self.controller, 'get_convent')
-#         self.mocks['verify_convent'].return_value = True
-#         self.mocks['get_convent'].return_value = fixtures['Convent']['first']
-#         self.controller.make()
+    @yield_fixture
+    def verify_convent(self, controller):
+        with patch.object(controller, 'verify_convent') as mock:
+            yield mock
 
-#     def test_make_verify(self):
-#         """StatisticsController should verify_convent and do nothing if it
-#         fails"""
-#         self.add_mock_object(self.controller, 'verify_convent')
-#         self.add_mock_object(self.controller, 'get_convent')
-#         self.mocks['verify_convent'].return_value = False
+    @yield_fixture
+    def get_convent(self, controller):
+        with patch.object(controller, 'get_convent') as mock:
+            yield mock
 
-#         self.controller.make()
+    def test_make(
+        self,
+        controller,
+        data,
+        verify_convent,
+        get_convent,
+        fixtures
+    ):
+        verify_convent.return_value = True
+        get_convent.return_value = fixtures['Convent']['first']
+        controller.make()
 
-#         self.assertEqual(0, self.mocks['get_convent'].call_count)
+    def test_make_verify(
+        self,
+        controller,
+        data,
+        verify_convent,
+        get_convent,
+        fixtures
+    ):
+        """
+        StatisticsController should verify_convent and do nothing if it
+        fails
+        """
+        verify_convent.return_value = False
+
+        controller.make()
+
+        assert not get_convent.called
