8e1cf09da61104d18e5a673a509194d33ebf8945
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-04-08 21:37:19 +0200

    Translation tests.

--- src/konwentor/application/tests/test_translations.py
+++ src/konwentor/application/tests/test_translations.py
@@ -1,13 +1,9 @@
-from haplugin.toster import TestCase
 from ..translations import KonwentorMessage
 
 
-class KonwentorMessageTests(TestCase):
-    prefix_from = KonwentorMessage
+class TestKonwentorMessage(object):
 
     def test_translate(self):
-        message = self.prefix_from()
+        message = KonwentorMessage()
         message.init('PasswordMustMatch')
-        self.assertEqual(
-            'Email i/lub hasło są błedne.',
-            message.translate())
+        assert message.translate() == 'Email i/lub hasło są błedne.'
