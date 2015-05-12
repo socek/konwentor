031b00e7b4fd32291311c6ab157bd05cd5edc82b
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-05-12 20:06:55 +0200

    Hints now works on rooms.

--- src/konwentor/application/static/js/game.borrows.js
+++ src/konwentor/application/static/js/game.borrows.js
@@ -1,31 +1,27 @@
-$(document).ready(function() {
-    function GatherHint(id) {
-        this.id = id;
-        this.form = $('#' + id);
-        this.name = $('#' + id + '_name');
-        this.surname = $('#' + id + '_surname');
-        this.number = $('#' + id + '_document_number');
-        this.document = $('#' + id + '_document_type');
+function GatherHint(id, room_id) {
+    this.id = id;
+    this.form = $('#' + id);
+    this.name = $('#' + id + '_name');
+    this.surname = $('#' + id + '_surname');
+    this.number = $('#' + id + '_document_number');
+    this.document = $('#' + id + '_document_type');
 
-        this.number.blur(function(){
-            $.post(
-                "/game/borrows/hint",
-                {
-                    number: this.number.val(),
-                }
-            ).done(function(data){
-                if(data.name != "") {
-                    this.name.val(data.name);
-                }
-                if(data.surname != "") {
-                    this.surname.val(data.surname);
-                }
-                if(data.document != "") {
-                    this.document.val(data.document);
-                }
-            }.bind(this));
+    this.number.blur(function(){
+        $.post(
+            "/game/"+ room_id + "/borrows/hint",
+            {
+                number: this.number.val(),
+            }
+        ).done(function(data){
+            if(data.name != "") {
+                this.name.val(data.name);
+            }
+            if(data.surname != "") {
+                this.surname.val(data.surname);
+            }
+            if(data.document != "") {
+                this.document.val(data.document);
+            }
         }.bind(this));
-    }
-
-    var hint = new GatherHint('GameBorrowAddForm');
-});
+    }.bind(this));
+}
